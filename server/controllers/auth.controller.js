345f96844fdb8f63537234748082fcf8ea03f0cf
NicolasBrazzo <nicolasbrazzo8@gmail.com>
2026-03-18 10:34:01 +0100

    fist commit

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { validateEmail } = require("../utils/validateEmail");
+const { validatePassword } = require("../utils/validatePassword");
+
+const { findUserByEmail, createNewUser } = require("../models/user.model");
+const protect = require("../middleware/auth");
+
+const { JWT_SECRET, JWT_EXPIRES_IN, SALT_ROUNDS } = require("../config/jwt");
+
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        ok: false,
+        error: "Email e password sono obbligatorie",
+      });
+    }
+
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        error: "Credenziali non valide",
+      });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(401).json({
+        ok: false,
+        error: "Credenziali non valide",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        sub: user.id,
+        email: user.email,
+        role: user.role,
+      },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN },
+    );
+
+    return res.json({
+      ok: true,
+      token,
+    });
+  } catch (err) {
+    console.error("LOGIN ERROR:", err);
+
+    return res.status(500).json({
+      ok: false,
+      error: "Errore interno del server",
+    });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, surname, email, password, isOrganizer } = req.body;
+
+    if (!name || !surname || !email || !password) {
+      return res.status(400).json({
+        ok: false,
+        error: "Nome, cognome, email e password sono obbligatori",
+      });
+    }
+
+    if (!validateEmail(email)) {
+      return res.status(400).json({
+        ok: false,
+        error: "Inserisci una email corretta",
+      });
+    }
+
+    const passwordErrors = validatePassword(password);
+    if (passwordErrors.length > 0) {
+      return res.status(400).json({
+        ok: false,
+        errors: passwordErrors,
+      });
+    }
+
+    const existingUser = await findUserByEmail(email);
+
+    if (existingUser) {
+      return res.status(409).json({
+        ok: false,
+        error: "Email già registrata",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const newUser = await createNewUser(
+      name,
+      surname,
+      email,
+      hashedPassword,
+      isOrganizer || false,
+    );
+
+    const token = jwt.sign(
+      {
+        sub: newUser.id,
+        email: newUser.email,
+        isOrganizer: newUser.isOrganizer,
+      },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN },
+    );
+
+    return res.status(201).json({
+      ok: true,
+      token,
+    });
+  } catch (err) {
+    console.error("REGISTER ERROR:", err);
+
+    return res.status(500).json({
+      ok: false,
+      error: "Errore interno del server",
+    });
+  }
+});
+
+router.get("/me", protect, (req, res) => {
+  return res.json({
+    ok: true,
+    user: req.user,
+  });
+});
+
+router.post("/logout", (req, res) => {
+  return res.json({ ok: true });
+});
+
+module.exports = router;
---
+++ server/models/user.model.js
@@ -0,0 +1,96 @@
+const supabase = require("../config/db_connection");
+
+// get all users
+const findAllUsers = async () => {
+  const { data, error } = await supabase.from("EFA_Users").select("*");
+  if (error) {
+    throw new Error("DATABASE_FIND_ALL_USERS_ERROR");
+  }
+  return data;
+};
+
+// create new user
+const createNewUser = async (name, surname, email, password, role) => {
+  const { data, error } = await supabase
+    .from("EFA_Users")
+    .insert([{ name, surname, email, password, isOrganizer }])
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error("DATABASE_CREATE_USER_ERROR");
+  }
+
+  return data;
+};
+
+// edit user by id
+const updateUserById = async (id, userData) => {
+  const { data, error } = await supabase
+    .from("EFA_Users")
+    .update(userData)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error("DATABASE_EDIT_USER_ERROR");
+  }
+
+  return data;
+};
+
+// delete user by id
+const deleteUserById = async (id) => {
+  const { data, error } = await supabase
+    .from("EFA_Users")
+    .delete()
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error("DATABASE_DELETE_USER_ERROR");
+  }
+
+  return data;
+};
+
+// find user by email
+const findUserByEmail = async (email) => {
+  const { data, error } = await supabase
+    .from("EFA_Users")
+    .select("*")
+    .eq("email", email)
+    .maybeSingle(); // .single genera un errore se non trova righe!
+
+  if (error) {
+    throw new Error("DATABASE_FIND_USER_ERROR");
+  }
+
+  return data;
+};
+
+// find user by id
+const findUserById = async (id) => {
+  const { data, error } = await supabase
+    .from("EFA_Users")
+    .select("*")
+    .eq("id", id)
+    .maybeSingle();
+
+  if (error) {
+    throw new Error("DATABASE_FIND_USER_ERROR");
+  }
+
+  return data;
+};
+
+module.exports = {
+  createNewUser,
+  findAllUsers,
+  findUserByEmail,
+  findUserById,
+  updateUserById,
+  deleteUserById,
+};
